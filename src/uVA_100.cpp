67acc4aa5c0d116d8d080d821ecf37970b0689e1
Danila <danila.atmakin@auto1.com>
2017-09-14 23:28:38 +0200

    re-format document

--- src/uVA_100.cpp
+++ src/uVA_100.cpp
@@ -6,8 +6,8 @@
 
 #define CACHE_MAX 10000
 
-namespace uVA_100 
-{    
+namespace uVA_100
+{
 
 int calculateCycleLength(int &number, long *temp, long *cycleLength)
 {
@@ -17,7 +17,8 @@ int calculateCycleLength(int &number, long *temp, long *cycleLength)
 TEST:
     count = count + 1;
     temp[step] = nextNum;
-    if (nextNum < CACHE_MAX && cycleLength[nextNum] > 0) {
+    if (nextNum < CACHE_MAX && cycleLength[nextNum] > 0)
+    {
         step = step + cycleLength[nextNum] - 1;
         goto FINISH;
     }
@@ -39,9 +40,11 @@ TEST:
             goto TEST;
         }
     }
-FINISH:    
-    for (long i2 = 1; i2 < count; i2++) {
-        if (temp[i2] < 10000){
+FINISH:
+    for (long i2 = 1; i2 < count; i2++)
+    {
+        if (temp[i2] < 10000)
+        {
             cycleLength[temp[i2]] = step - i2 + 1;
         }
     }
@@ -49,7 +52,7 @@ FINISH:
 }
 
 int calculateMaxCycleLengthForRange(int start, int end, long *temp, long *cycleLength)
-{    
+{
     int actualStart = start;
     int actualEnd = end;
     if (start > end)
@@ -60,7 +63,7 @@ int calculateMaxCycleLengthForRange(int start, int end, long *temp, long *cycleL
 
     int maxLength = 0;
     for (int number = actualStart; number <= actualEnd; number++)
-    {    
+    {
         int currentLength = calculateCycleLength(number, temp, cycleLength);
         if (currentLength > maxLength)
         {
@@ -71,14 +74,13 @@ int calculateMaxCycleLengthForRange(int start, int end, long *temp, long *cycleL
 }
 }
 
-
 int main()
-{        
-    int i, j;        
+{
+    int i, j;
     long cycleLength[CACHE_MAX] = {0};
     long temp[CACHE_MAX] = {0};
     while (scanf("%d %d", &i, &j) != EOF)
-    {        
+    {
         int result = uVA_100::calculateMaxCycleLengthForRange(i, j, temp, cycleLength);
         printf("%d %d %d\n", i, j, result);
     }
