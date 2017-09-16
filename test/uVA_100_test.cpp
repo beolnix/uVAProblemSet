367f224dd7892efe016c7ffdbad388bee33f00ee
Danila <danila.atmakin@auto1.com>
2017-09-16 23:11:37 +0200

    more tests in testcase for uVA_100

--- test/uVA_100_test.cpp
+++ test/uVA_100_test.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE "BaseClassModule"
+#define BOOST_TEST_MODULE "uVA100TestCase"
 
 // libs
 #include <boost/test/unit_test.hpp>
@@ -9,19 +9,35 @@
 
 #define CACHE_MAX 10000
 
-//Name your test cases for what they test
-BOOST_AUTO_TEST_CASE(assignment)
+BOOST_AUTO_TEST_SUITE(generic_tests);
+
+long cycleLength[CACHE_MAX] = {0};
+long temp[CACHE_MAX] = {0};
+long cacheMax = CACHE_MAX;
+
+void verifyResultForRange(int expectedResult, int i, int j) {
+    int actualResult = uVA_100::calculateMaxCycleLengthForRange(&i, &j, cycleLength, temp, &cacheMax);
+    BOOST_CHECK_EQUAL(actualResult, expectedResult);
+    
+    char buff[100];
+    snprintf(buff, sizeof(buff), "[SUCCESS] i: %d, j:%d, result:%d", i, j, expectedResult);
+    BOOST_TEST_MESSAGE(buff);
+    BOOST_TEST_PASSPOINT();
+}
+
+BOOST_AUTO_TEST_CASE(given_test_data)
 {
 
-    //Use BOOST_CHECK_EQUAL if you want the values to be
-    //printed out if they mismatch
-    long cycleLength[CACHE_MAX] = {0};
-    long temp[CACHE_MAX] = {0};
-    long cacheMax = CACHE_MAX;
-    int start = 1;
-    int end = 10;
-    int result = uVA_100::calculateMaxCycleLengthForRange(&start, &end, cycleLength, temp, &cacheMax);
-    BOOST_CHECK_EQUAL(result, 20);
+    // if start of the range is lower then beginning of a range
+    verifyResultForRange(20, 1, 10);    
 
-    // BOOST_CHECK(true);
+    // if start of the range greater then beginning of a range - result must be valid anyway
+    verifyResultForRange(20, 10, 1);
+
+    // other examples of a sample input
+    verifyResultForRange(125, 100, 200);
+    verifyResultForRange(89, 201, 210);
+    verifyResultForRange(174, 900, 1000);
 }
+
+BOOST_AUTO_TEST_SUITE_END();
